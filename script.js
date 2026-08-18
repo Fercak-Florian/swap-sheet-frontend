5a7aa0a9455b0290f53003f15ef7f47d50c41a3c
Florian Fercak <fercak.florian@gmail.com>
2026-08-18 09:06:11 +0200

    landing page added

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ async function getData() {
   const url = "http://127.0.0.1:8080";
   try {
     const reponse = await fetch(url);
-    console.log(reponse);
     if (!reponse.ok) {
       throw new Error(`Statut de réponse : ${reponse.status}`);
     }
 
-    const resultat = await reponse.json();
-    console.log(resultat);
+    const allTsp = await reponse.json();
+    return allTsp
   } catch (erreur) {
     console.error(erreur.message);
   }
 }
+// Start //
+console.log("en cours...")
+const allTsp = await getData();
+getSelectOptions(allTsp);
 
-getData();
+
+
+function getSelectOptions(allTsp) {
+  const tspSelect = document.getElementById("tsp-select");
+    for (let i = 0; i < allTsp.length; i++) {
+      console.log(allTsp[i]);
+      const optionElement = document.createElement("option");
+      optionElement.textContent = allTsp[i]["lastName"] + " " + allTsp[i]["firstName"];
+      tspSelect.appendChild(optionElement);
+  }
+}
